e05ac9babc004bf9aa8c20b5de62ea601196ef09
Ooi Liang Sun <liangsun@gmail.com>
2022-01-22 13:59:16 +0800

    initial commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir conf storage plugins
+
+V_PATH=`pwd`; docker run -d --name verdaccio \
+  -p 4873:4873 \
+  -v $V_PATH/conf:/verdaccio/conf \
+  -v $V_PATH/storage:/verdaccio/storage \
+  -v $V_PATH/plugins:/verdaccio/plugins \
+  verdaccio/verdaccio:5.4
